9061634f5287df0630bb14f09dfb4c0d2595efc4
bamss <amadougolfa98@gmail.com>
2019-01-17 15:19:43 +0100

    Trader sombre terminé

--- lib/trader_sombre.rb
+++ lib/trader_sombre.rb
@@ -1,2 +1,25 @@
 require 'nokogiri'
 require 'open-uri'
+PAGE_URL = "https://coinmarketcap.com/all/views/all/"
+
+
+
+def my_billion_crypto
+
+page = Nokogiri::HTML(open(PAGE_URL))
+coins = []
+page.xpath('//tr/td[3]').each do |node|
+    puts node.text
+    coins.push(node.text)
+ end
+ 
+ values = []
+ page.xpath('//td/a[@class="price"]').each do |node|
+   puts node.text
+  values.push(node.text)
+ end
+ 
+ trader = Hash[coins.zip(values)]
+ p trader
+
+end
---
+++ spec/trader_sombre_spec.rb
@@ -0,0 +1,7 @@
+require_relative '../lib/trader_sombre.rb'
+
+describe "my_billion_crypto" do
+    it "should return a Array Hash" do
+        expect(my_billion_crypto).to be_truthy
+    end
+end
