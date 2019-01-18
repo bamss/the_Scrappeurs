4657771f2791145758e50a911422f990f8100242
RobinsonDbs <46196386+RobinsonDbs@users.noreply.github.com>
2019-01-18 10:14:47 +0100

    Update trader_sombre_spec.rb

--- spec/trader_sombre_spec.rb
+++ spec/trader_sombre_spec.rb
@@ -3,5 +3,6 @@ require_relative '../lib/trader_sombre.rb'
 describe "my_billion_crypto" do
     it "should return an array" do
         expect(my_billion_crypto).to be_truthy
+        expect(my_billion_crypto).to have_at_least(50).items
     end
 end
