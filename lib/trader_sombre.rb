573aad06f1d8e19f70688f19def2a1e5090331fb
Mr_Golfa <amadougolfa98@gmail.com>
2019-01-18 09:42:14 +0100

    Update trader_sombre.rb

--- lib/trader_sombre.rb
+++ lib/trader_sombre.rb
@@ -5,20 +5,20 @@ PAGE_URL = "https://coinmarketcap.com/all/views/all/"
 
 
 def my_billion_crypto
-
+#recupère le nom des cryptos qu'on stocke dans coins
 page = Nokogiri::HTML(open(PAGE_URL))
 coins = []
 page.xpath('//tr/td[3]').each do |node|
     puts node.text
     coins.push(node.text)
  end
- 
+ #recupère le cours des cruptos qu'on stocke dans Values
  values = []
  page.xpath('//td/a[@class="price"]').each do |node|
    puts node.text
   values.push(node.text)
  end
- 
+ # là on crée un tableau de hash avec les arrays coins et values
  trader = Hash[coins.zip(values)]
  p trader
 
